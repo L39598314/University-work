108248a04d5d8de8d22b9903af404f20ca3589d8
Hena <l39598314@student.ua92.ac.uk>
2025-02-20 22:22:10 +0000

    Delete Javascript code directory

--- Javascript code/output.js
+++
@@ -1,9 +0,0 @@
-const Name = localStorage.getItem('Name');
-            const Email = localStorage.getItem('Email');
-            document.getElementById('Name').textContent = Name;
-            document.getElementById('EmailAddress').textContent = Email;
-
-/* the source of this code comes a youtube video who explained how to get information
-and store the name and email address properly in javascript
-    here is the link to the youtube video:
-   https://www.youtube.com/watch?v=x0VcigW9kN0   */
